cbf2ec18689cf802236fcd3ebecd1b62160b1ac5
himanshuvarshney29 <himanshuvarshney29@gmail.com>
2021-04-26 21:14:01 +0530

    new changes

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('users', 'UserController@getUsers');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::get('/', 'UserController@getUsers');
